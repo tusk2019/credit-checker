25ad09aa9e9c93d5879dceb46c16417d5dfabf53
tusk2019 <kouki032812@gmail.com>
2022-05-10 14:11:47 +0900

    ログインの仕組みをpassword_verify関数を用いたものに変更

--- cls/login.php
+++ cls/login.php
@@ -14,7 +14,7 @@ $_SESSION['ID']=$sid;
 
 //POSTデータ取得(未完成)
 if (!isset($_POST['login_id'])) {
-    //店舗IDが未設定時、ログインページへ戻す
+    //IDが未設定時、ログインページへ戻す
     header("Location: ../index.php?type=0");
     exit;
 }
@@ -27,18 +27,19 @@ if (!isset($_POST['password'])) {
 $login_id = null;
 $password = null;
 
-$login_id = pg_escape_string(htmlspecialchars($_POST['login_id'], ENT_QUOTES));
-$password = pg_escape_string(htmlspecialchars($_POST['password'], ENT_QUOTES));
+//$login_id = pg_escape_string(htmlspecialchars($_POST['login_id'], ENT_QUOTES));
+$login_id = htmlspecialchars($_POST['login_id'], ENT_QUOTES);
+//$password = pg_escape_string(htmlspecialchars($_POST['password'], ENT_QUOTES));
+$password = htmlspecialchars($_POST['password'], ENT_QUOTES);
 
 //セッションにユーザーIDとユーザー名を入れる
 
 try {
     // DBへ接続
-    $dbh = new PDO("pgsql:host=127.0.0.1; dbname=booktown;", 'booktown', 'kouki0328');
+    require("dbconnect.php");
 
     //ログイン画面でpostされたログインIDとパスワードをaccountテーブルで検索し、該当すればそのlogin_id, passwordを取得
-    $sql = "SELECT serial_no, user_name, login_id, password FROM users WHERE login_id = '" .$login_id. "' AND password = '" .$password. "'
-    AND delete_flg = 0";
+    $sql = "SELECT serial_no, user_name, login_id, password FROM users WHERE login_id = '" .$login_id. "' AND delete_flg = 0";
     $sth = $dbh->prepare($sql);
     $sth->execute();
     $data = $sth->fetch();
@@ -49,20 +50,16 @@ try {
       //存在しない（ログインページに戻る）
       header("Location: ../index.html?error=1");
     } else {
-      // $data = $sth->fetch();
-      // var_dump($data);
-      // exit();
-      //データ
-      // foreach ($data as $value) {
-      //   $user_id = $value['serial_no'];
-      //   $_SESSION['USERID'] = $user_id;
-      // }
+      if( password_verify($password, $data['password']) ) {
       $user_id = array_shift($data);
       $_SESSION['USERID'] = $user_id;
-      // $user_name = $data['user_name'];
-      // $_SESSION['USERNM'] = $user_name;
+      $_SESSION['LOGIN_ID'] = $data['login_id'];
       //後払いリストへ移動
       header("Location: ../pages/page_credit_list.php");
+      }else {
+        //存在しない（ログインページに戻る）
+        header("Location: ../index.html?error=1");
+      }
     }
 } catch (PDOException $e) {
     echo $e->getMessage();
